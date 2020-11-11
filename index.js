7bec7ff0c3b2e420a64cd3ef3fdd5c0385e36b6c
smadhavapeddi <68992423+smadhavapeddi@users.noreply.github.com>
2020-11-11 14:45:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World! This is my first Nodejs App using PM2'));
 
 // Wait a full 2 seconds before logging so that we can be sure
 // that pm2 is listening and not swallowing logs
